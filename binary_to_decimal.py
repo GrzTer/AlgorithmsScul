729aa1287908607b7d5fa1c3b8c8303f0cabb26b
GrzegorzT <144318154+GrzTer@users.noreply.github.com>
2025-10-23 14:28:04 +0200

    Update binary_to_decimal.py

--- binary_to_decimal.py
+++ binary_to_decimal.py
@@ -1,4 +1,32 @@
-def binary_to_decimal(binary_string: str) -> int:
+reszty = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
+def z10naSystem(liczba, system):
+    wynik = ""
+    while liczba > 0:
+        reszta = liczba % system
+        wynik = reszty[reszta] + wynik
+        liczba = liczba //system
+    return wynik
+
+print(z10naSystem(30, 16))
+
+
+def zSystemNa10(tekst, system):
+    # schemat Hornera
+    wynik = 0
+    for znak in tekst:
+        wynik = wynik * system + reszty.index(znak)
+    return wynik
+        
+print(zSystemNa10("1E", 16))  
+
+
+
+
+
+
+
+def binary_to_decimal(binary_string: str) -> 
+int:
     decimal_number = 0
     for i, digit in enumerate(binary_string[::-1]):
         decimal_number += int(digit) * (2**i)
