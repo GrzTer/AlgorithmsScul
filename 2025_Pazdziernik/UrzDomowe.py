3207ac0b36ebaff8ef5e4d4ee0818e7a37a980b9
GrzTer <144318154+GrzTer@users.noreply.github.com>
2025-10-31 18:22:49 +0100

    .

---
+++ 2025_Pazdziernik/UrzDomowe.py
@@ -0,0 +1,18 @@
+class Urzadzenie:
+    def pokaz_komunikat(self, komunikat: str) -> None: print(komunikat)
+class Pralka(Urzadzenie):
+    def __init__(self):
+        self.__numer_programu = 0
+
+    def ustaw_program(self, numer_programu) -> int:
+        if 1 <= numer_programy
+
+
+class Pralka(Urzadzenie):
+    ...
+
+def main() -> None:
+
+
+if __name__ == "__main__":
+    main()
